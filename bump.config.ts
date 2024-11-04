8bde1489170bdabed8b6600d937ef5fa2c639f0e
mkpoli <git@mkpo.li>
2024-11-04 18:14:31 +0900

    Commit changelog update before bump

--- bump.config.ts
+++ bump.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
 	push: false,
 	tag: true,
 	commit: "Bump version to v%s",
-	execute: "bun run update-changelog && git add CHANGELOG.md",
+	execute:
+		"bun run update-changelog && git add CHANGELOG.md && git commit -m 'Update CHANGELOG.md'",
 });
