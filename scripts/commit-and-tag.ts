31b1250e7b85aadf274a4a25efe6528916187c57
mkpoli <git@mkpo.li>
2024-11-28 18:12:57 +0900

    End commit-and-tag process

--- scripts/commit-and-tag.ts
+++ scripts/commit-and-tag.ts
@@ -4,3 +4,5 @@ const npmPackageVersion = (await Bun.file("package.json").json()).version;
 
 await $`git commit -am "Bump version to v${npmPackageVersion}"`;
 await $`git tag -a v${npmPackageVersion} -m "Bump version to v${npmPackageVersion}"`;
+
+process.exit(0);
